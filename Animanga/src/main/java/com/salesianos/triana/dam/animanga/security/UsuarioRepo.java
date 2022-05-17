8f6dfab0fed1c3457f93a3c5ccfd73e2180dd544
Arturo Céspedes Pedrazas <Arturo200212@gmail.com>
2022-05-17 14:21:18 +0200

    Commit

--- Animanga/src/main/java/com/salesianos/triana/dam/animanga/security/UsuarioRepo.java
+++ Animanga/src/main/java/com/salesianos/triana/dam/animanga/security/UsuarioRepo.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class UsuarioRepo {
-	
+
 	private List<Usuario> usuarios;
 
 	public List<Usuario> getUsuarios() {
@@ -19,37 +19,19 @@ public class UsuarioRepo {
 	}
 
 	public Optional<Usuario> findUserByUsername(String username) {
-		return usuarios.stream()
-				.filter(u -> u.getUsername().equals(username))
-				.findFirst();
+		return usuarios.stream().filter(u -> u.getUsername().equals(username)).findFirst();
 	}
-	
+
 	@PostConstruct
 	public void init() {
 		usuarios = List.of(
-				Usuario.builder()
-					.username("admin")
-					.password("admin")
-					.role("ADMIN")
-					.nombre("Arturo")
-					.apellidos("Céspedes Pedrazas")
-					.fechaNacimiento(LocalDate.of(2002, 2, 4))
-					.build()
-				,
-				Usuario.builder()
-				.username("user")
-				.password("user")
-				.role("USER")
-				.nombre("Usuario ")
-				.apellidos("Normal")
-				.fechaNacimiento(LocalDate.of(2000,4,23))
-				.build()
-				
-				
-				);
-				
+				Usuario.builder().username("admin").password("admin").role("ADMIN").nombre("Arturo")
+						.apellidos("Céspedes Pedrazas").fechaNacimiento(LocalDate.of(2002, 2, 4)).build(),
+				Usuario.builder().username("user").password("user").role("USER").nombre("Usuario ").apellidos("Normal")
+						.fechaNacimiento(LocalDate.of(2000, 4, 23)).build()
+
+		);
+
 	}
-	
-	
 
 }
